2619d7e62bab17b7b0e56a46c070b67e1790492c
gitee-wangliang <1991wangliang@gmail.com>
2020-01-07 10:34:42 +0800

    .codecov

--- demo/src/test/java/com/example/springboot/demo/service/impl/DemoServiceImplTest.java
+++ demo/src/test/java/com/example/springboot/demo/service/impl/DemoServiceImplTest.java
@@ -1,22 +1,32 @@
 package com.example.springboot.demo.service.impl;
 
+import com.example.springboot.demo.feign.MessageClient;
+import com.example.springboot.demo.manager.RefrigeratorManager;
 import com.example.springboot.demo.pojo.vo.AnimalReq;
 import com.example.springboot.demo.pojo.vo.AnimalRes;
 import com.example.springboot.demo.service.DemoService;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.util.Assert;
 
 @SpringBootTest
 @Slf4j
-@Rollback
 class DemoServiceImplTest {
 
     @Autowired
+    private RefrigeratorManager refrigeratorManager;
+
     private DemoService demoService;
+
+    @BeforeEach
+    public void before(){
+        MessageClient messageClient = Mockito.mock(MessageClient.class);
+        demoService = new DemoServiceImpl(refrigeratorManager,messageClient);
+    }
 
     @Test
     void put() {
